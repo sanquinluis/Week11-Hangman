00580780418faf71e4999319c675a8fc4ddf080e
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-09-10 18:39:38 -0400

    Some updates

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-//CONSTRUCTOR FUNCTION
+//CONSTRUCTOR FUNCTION FOR GAME.JS
 //Instruction:
 //Your game.js file will randomly select a word for the player.
 var Game = function(){
--- letter.js
+++ letter.js
@@ -1,23 +1,21 @@
 //CONSTRUCTOR FUNCTION FOR LETTER.JS
 //Instructions:
 //Letter.js Should control whether or not a letter appears as a "_" or as itself on-screen.
-var letras = process.argv[3];
-console.log(letras);
+
 var Letter = function(letras){
 
 	this.theLetters = letras;
 	//letter appears
 	this.shows = false;
-	//Method that will tell us if the letter is in the word.
+	//Method that will tell us if the letter appears or if "_" will appear.
 	this.letterShows = function(theLetters){
-		console.log("The letter is inside letterShows" + theLetters);
-		if(this.theLetters === false){
-			return "_";
-		} else {
+		console.log("letterShows" + theLetters);
+		if(theLetters === this.theLetters){
 			return theLetters;
-	}
-}
-var test = new Letter("test");
-var x = test.letterShows(theLetters);
-console.log(x + "test");
+		} else {
+			return "_";
+		}
+	};
+};
+
 module.exports = Letter;
